f0781942abc020682ef3895ad6367a3cb61e11af
ysicing <i@ysicing.me>
2019-05-05 16:18:38 +0800

    Update download.sh

--- download/centos-7.4/download.sh
+++ download/centos-7.4/download.sh
@@ -2,10 +2,10 @@
 
 PKG_PATH=/opt/rainbond/install/install/pkgs
 
-base_pkg=(yum-utils tar jq wget curl nano perl tree lsof htop nload net-tools telnet rsync git ntp ntpdate pwgen iproute bash-completion createrepo sshpass python2-pip pwgen expect ansible)
+base_pkg=(yum-utils tar jq wget curl nano perl tree lsof htop nload net-tools telnet rsync git ntp ntpdate pwgen iproute bash-completion createrepo sshpass python2-pip pwgen expect ansible createrepo zip unzip )
 system_pkg=(device-mapper-persistent-data conntrack-tools iotop lvm2 dstat psmisc socat chrony ipset ipvsadm bind-utils keepalived )
 network_pkg=(bridge-utils )
-storage_pkg=(glusterfs-server nfs-utils )
+storage_pkg=(glusterfs-server nfs-utils samba samba-client samba-common )
 docker=(docker-ce )
 
 if [ "$1" == "bash" ];then
